6522c31ea3e0ce4d74d1443750493637918fe11c
igorfcfs <igorfcfs@gmail.com>
2023-12-06 08:18:40 -0300

    Introdução à Threads

---
+++ threads/src/threads/ImprimeString.java
@@ -0,0 +1,9 @@
+package threads;
+
+public class ImprimeString implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("resposta");
+    }
+}
---
+++ threads/src/threads/Principal.java
@@ -0,0 +1,9 @@
+package threads;
+
+public class Principal {
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("essa é a thread main");
+		
+		Thread.sleep(50000);
+	}
+}
