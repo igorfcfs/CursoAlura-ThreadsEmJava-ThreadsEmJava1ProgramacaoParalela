727c93bf580af245166b966478014162d9fe4adb
igorfcfs <igorfcfs@gmail.com>
2023-12-06 15:40:59 -0300

    Espere e Notifique

--- banheiro/src/br/com/alura/banheiro/Banheiro.java
+++ banheiro/src/br/com/alura/banheiro/Banheiro.java
@@ -1,48 +1,99 @@
 package br.com.alura.banheiro;
 
 public class Banheiro {
-	
-	public void fazNumero1() { //este metodo realiza uma operacao atomica: cuja execucao nao pode ser interrompida pela metade
+
+	private boolean ehSujo = true;
+
+	public void fazNumero1() { // este metodo realiza uma operacao atomica: cuja execucao nao pode ser
+								// interrompida pela metade
 		String nome = Thread.currentThread().getName();
-		
+
 		System.out.println(nome + " batendo na porta");
-		
-		synchronized(this/*"chave que tranca o banheiro" - Mutex*/) { //"trancar o banheiro"
-			//esse bloco pode ser executado por uma thread apenas
+
+		synchronized (this/* "chave que tranca o banheiro" - Mutex */) { // "trancar o banheiro"
+			// esse bloco pode ser executado por uma thread apenas
 			System.out.println(nome + " entrando no banheiro");
+
+			while(ehSujo) {
+				esperaLaFora(nome);
+			}
+
 			System.out.println(nome + " fazendo coisa rapida");
+
+			dormeUmPouco(5000);
 			
-			try {
-				Thread.sleep(5000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			
-			System.out.println(nome + " dando descarga");
-			System.out.println(nome + " lavando as maos");
-			System.out.println(nome + " saindo do banheiro");
-		}
-	}
-	public void fazNumero2() { //este metodo realiza uma operacao atomica: cuja execucao nao pode ser interrompida pela metade
-		String nome = Thread.currentThread().getName();
-		
-		System.out.println(nome + " batendo na porta");
-		
-		synchronized(this/*"chave que tranca o banheiro" - Mutex*/) { //"trancar o banheiro"
-			//esse bloco pode ser executado por uma thread apenas
-			System.out.println(nome + " entrando no banheiro");
-			System.out.println(nome + " fazendo coisa demorada");
-			
-			try {
-				Thread.sleep(10000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			
+			this.ehSujo = true;
+
 			System.out.println(nome + " dando descarga");
 			System.out.println(nome + " lavando as maos");
 			System.out.println(nome + " saindo do banheiro");
 		}
 	}
 
+	public void fazNumero2() { // este metodo realiza uma operacao atomica: cuja execucao nao pode ser
+								// interrompida pela metade
+		String nome = Thread.currentThread().getName();
+
+		System.out.println(nome + " batendo na porta");
+
+		synchronized (this/* "chave que tranca o banheiro" - Mutex */) { // "trancar o banheiro"
+			// esse bloco pode ser executado por uma thread apenas
+			System.out.println(nome + " entrando no banheiro");
+			
+			while(ehSujo) {
+				esperaLaFora(nome);
+			}
+			
+			System.out.println(nome + " fazendo coisa demorada");
+			
+			dormeUmPouco(10000);
+			
+			this.ehSujo = true;
+
+			System.out.println(nome + " dando descarga");
+			System.out.println(nome + " lavando as maos");
+			System.out.println(nome + " saindo do banheiro");
+		}
+	}
+
+	private void dormeUmPouco(long millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void limpa() { //a pessoa que limpa o banheiro so pode entrar quando alguem terminar de usa-lo, por isso a palavra chave synchronized
+		String nome = Thread.currentThread().getName();
+
+		System.out.println(nome + " batendo na porta");
+		
+		synchronized (this) {
+			System.out.println(nome + " entrando no banheiro");
+			
+			if(!ehSujo) {
+				System.out.println(nome + ", não está sujo, vou sair");
+				return; // termina o metodo
+			}
+			
+			System.out.println(nome + " limpando banheiro");
+			this.ehSujo = false;
+			
+			dormeUmPouco(13000);
+			
+			this.notifyAll();
+			
+			System.out.println(nome + " saindo do banheiro");
+		}
+	}
+	
+	private void esperaLaFora(String nome) {
+		System.out.println(nome + ", eca, banheiro tá sujo");
+		try {
+			this.wait();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- banheiro/src/br/com/alura/banheiro/Principal.java
+++ banheiro/src/br/com/alura/banheiro/Principal.java
@@ -10,12 +10,16 @@ public class Principal {
 		
 		Thread convidado1 = new Thread(new TarefaNumero1(banheiro), "Joao");
 		Thread convidado2 = new Thread(new TarefaNumero2(banheiro), "Pedro");
-		Thread convidado3 = new Thread(new TarefaNumero1(banheiro), "Ana");
-		Thread convidado4 = new Thread(new TarefaNumero2(banheiro), "Maria");
+//		Thread convidado3 = new Thread(new TarefaNumero1(banheiro), "Ana");
+//		Thread convidado4 = new Thread(new TarefaNumero2(banheiro), "Maria");
+		Thread limpeza = new Thread(new TarefaLimpeza(banheiro), "Limpeza");
+		
+		limpeza.setDaemon(true); // Uma thread daemon é uma prestadora de serviços para outras threads. Ela só é usada enquanto as outras threads estão rodando.
 		
 		convidado1.start();
 		convidado2.start();
-		convidado3.start();
-		convidado4.start();
+//		convidado3.start();
+//		convidado4.start();
+		limpeza.start();
 	}
 }
